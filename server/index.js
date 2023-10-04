efccc4b6741f17855b155f2b2cf816e0e989e1b6
Juda0 <judadevries@gmail.com>
2023-10-04 17:24:17 +0200

    Fetch posts from API

--- server/index.js
+++ server/index.js
@@ -1,5 +1,12 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
+
+// Allow following external request to the server:
+app.use(cors({
+  origin: 'http://localhost:3000',
+}));
+
 
 app.get("/", (req, res) => {
   res.status(201).send("Listening on port 4000")
@@ -11,4 +18,6 @@ const claimRouter = require("./routes/claims")
 app.use("/posts", postRouter)
 app.use("/claims", claimRouter)
 
-app.listen(4000)
+app.listen(4000, () => {
+  console.log("Listen to port 4000");
+});
