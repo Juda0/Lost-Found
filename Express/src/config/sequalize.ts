013c99dd2e57a8f29449f2e58e5a8a4fb95094d3
Juda0 <judadevries@gmail.com>
2023-11-09 17:04:03 +0100

    Prisma migration added

--- Express/src/config/sequalize.ts
+++
@@ -1,8 +0,0 @@
-// sequelize.ts
-import { Sequelize } from 'sequelize';
-import config from './config.json';
-
-const env = process.env.NODE_ENV || 'development';
-const sequelize = new Sequelize(config[env]);
-
-export default sequelize;
